7344d85655fac053d796a92cc290c23c6cc4e0c4
jsf5077 <jsf5077@gmail.com>
2019-09-15 18:53:17 -0400

    modifications to frontend login and registration

--- client/src/components/Registration.js
+++ client/src/components/Registration.js
@@ -5,25 +5,25 @@ import Form from 'react-bootstrap/Form';
 function Registration() {
   return (
     <Form>
-        <Form.Group controlId="formGroupFirstName">
-        <Form.Label>First Name</Form.Label>
-        <Form.Control type="firstName" placeholder="Enter First Name" />
-      </Form.Group>
-      <Form.Group controlId="formGroupLastName">
-        <Form.Label>Last Name</Form.Label>
-        <Form.Control type="lastName" placeholder="Enter Last Name" />
+        <Form.Group controlId="formGroupName">
+        <Form.Label>Name:</Form.Label>
+        <Form.Control type="firstName" placeholder="Enter Name" />
       </Form.Group>
       <Form.Group controlId="formGroupUsername">
-        <Form.Label>Username</Form.Label>
+        <Form.Label>Username:</Form.Label>
         <Form.Control type="username" placeholder="Enter Username" />
       </Form.Group>
       <Form.Group controlId="formGroupEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Label>Email address:</Form.Label>
+        <Form.Control type="email" placeholder="Enter Email Address" />
       </Form.Group>
       <Form.Group controlId="formGroupPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Label>Password:</Form.Label>
+        <Form.Control type="password" placeholder="Enter New Password" />
+      </Form.Group>
+      <Form.Group controlId="formGroupPassword2">
+        <Form.Label>Enter Password Again:</Form.Label>
+        <Form.Control type="password2" placeholder="Confirm Password" />
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit

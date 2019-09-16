f1fb024ab1c2b422e2ae66404f7787e4f9ab8ae9
jsf5077 <jsf5077@gmail.com>
2019-09-16 18:53:08 -0400

    updated branch for Keith

--- client/src/components/Registration.js
+++ client/src/components/Registration.js
@@ -1,37 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-const register = require("../../controllers/registerController");
+// const register = require("../../controllers/registerController");
 
-function Registration() {
-  return (
-    <Form>
-        <Form.Group controlId="formGroupName">
-        <Form.Label>Name:</Form.Label>
-        <Form.Control type="firstName" placeholder="Enter Name" />
-      </Form.Group>
-      <Form.Group controlId="formGroupUsername">
-        <Form.Label>Username:</Form.Label>
-        <Form.Control type="username" placeholder="Enter Username" />
-      </Form.Group>
-      <Form.Group controlId="formGroupEmail">
-        <Form.Label>Email address:</Form.Label>
-        <Form.Control type="email" placeholder="Enter Email Address" />
-      </Form.Group>
-      <Form.Group controlId="formGroupPassword">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control type="password" placeholder="Enter New Password" />
-      </Form.Group>
-      <Form.Group controlId="formGroupPassword2">
-        <Form.Label>Enter Password Again:</Form.Label>
-        <Form.Control type="password2" placeholder="Confirm Password" />
-      </Form.Group>
-      <Button variant="primary" id="registerButton" onClick={() => {register} } type="submit">
-        Submit
-      </Button>
-    </Form>
-  );
+class Registration extends Component {
+    register=()=> {
+      
+    }
+  
+  render() {
+    return (
+      <Form>
+          <Form.Group controlId="formGroupName">
+          <Form.Label>Name:</Form.Label>
+          <Form.Control type="firstName" placeholder="Enter Name" />
+        </Form.Group>
+        <Form.Group controlId="formGroupUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control type="username" placeholder="Enter Username" />
+        </Form.Group>
+        <Form.Group controlId="formGroupEmail">
+          <Form.Label>Email address:</Form.Label>
+          <Form.Control type="email" placeholder="Enter Email Address" />
+        </Form.Group>
+        <Form.Group controlId="formGroupPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control type="password" placeholder="Enter New Password" />
+        </Form.Group>
+        <Form.Group controlId="formGroupPassword2">
+          <Form.Label>Enter Password Again:</Form.Label>
+          <Form.Control type="password2" placeholder="Confirm Password" />
+        </Form.Group>
+        <Button variant="primary" id="registerButton" 
+        onClick={() => this.register } 
+        type="submit">
+          Submit
+        </Button>
+      </Form>
+    );
+  }
 }
 
 export default Registration;

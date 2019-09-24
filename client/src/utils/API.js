c0ca8a59501a13bd6257d333be00f1b7dfa54263
Karen Largent <karenlargent@Karens-MacBook-Pro.local>
2019-09-24 19:39:46 -0400

    saving the user info and token in local storage so the FE can retrieve it

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -11,8 +11,17 @@ export default {
   postLogin: function(loginUser) {
     JSON.stringify(loginUser); 
     console.log("i logged in"); 
-    return axios.post("/api/auth/login", loginUser);
-    // console.log(loginUser); 
+    axios.post("/api/auth/login", loginUser)
+    .then (function (loggedIn) {
+      // JSON.stringify(loginUser); 
+      console.log(loggedIn); 
+      var user = {
+        user: loggedIn.data.user.id, 
+        token: loggedIn.data.token
+      }
+      console.log(user); 
+      localStorage.setItem("user", JSON.stringify(user)); 
+    })
     
   }
 };

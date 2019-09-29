5aba27c6d0ef91a34d8416317d89da06af773ae3
Pam Tholan <pamelatholan@yahoo.com>
2019-09-29 14:36:21 -0400

    added table and started to troubleshoot connection to DB

--- client/src/components/DataDisplay.js
+++ client/src/components/DataDisplay.js
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import Container from "react-bootstrap/Container";
 import Table from "react-bootstrap/Table";
 import "./styles/style.css";
+// import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-class DataDisplay extends Component {
+// class DataDisplay extends Component {
 
   // handleLoad = event => {
   //   // const token = serverResult;
@@ -16,36 +18,91 @@ class DataDisplay extends Component {
   //   console.log(loginUser);
   // };
 
+//   render() {
+//     return (
+//       <Container>
+//         <Table striped bordered hover variant="dark">
+//           <thead>
+//             <tr>
+//               <th>Lift Name</th>
+//               <th>Reps</th>
+//               <th>PR</th>
+//               <th>Date</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             <tr>
+//               <td>props.whatever</td>
+//               <td>props.whatever</td>
+//               <td>props.whatever</td>
+//               <td>props.whatever</td>
+//             </tr>
+//             <tr>
+//               <td>props.whatever</td>
+//               <td>props.whatever</td>
+//               <td>props.whatever</td>
+//               <td>props.whatever</td>
+//             </tr>
+//           </tbody>
+//         </Table>
+//       </Container>
+//     );
+//   }
+// }
+
+
+const Lifts = props => (
+  <tr>
+      <td>{props.lifts.lifts_liftName}</td>
+      <td>{props.lifts.lifts_reps}</td>
+      <td>{props.lifts.lifts_pr}</td>
+      <td>{props.lifts.lifts_date}</td>
+      <td>
+          {/* <Link to={"/edit/"+props.lifts._id}>Edit</Link> */}
+      </td>
+  </tr>
+)
+export default class DataDisplay extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {lifts: []};
+  }
+  componentDidMount() {
+      axios.get('http://localhost:3001/dashboard/')
+          .then(response => {
+              this.setState({ lifts: response.data });
+          })
+          .catch(function (error){
+              console.log(error);
+          })
+  }
+  liftList() {
+      return this.state.lifts.map(function(currentLift, i){
+          return <Lifts lift={currentLift} key={i} />;
+      })
+  }
   render() {
-    return (
-      <Container>
-        <Table striped bordered hover variant="dark">
-          <thead>
-            <tr>
-              <th>Lift Name</th>
-              <th>Reps</th>
-              <th>PR</th>
-              <th>Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>props.whatever</td>
-              <td>props.whatever</td>
-              <td>props.whatever</td>
-              <td>props.whatever</td>
-            </tr>
-            <tr>
-              <td>props.whatever</td>
-              <td>props.whatever</td>
-              <td>props.whatever</td>
-              <td>props.whatever</td>
-            </tr>
-          </tbody>
-        </Table>
-      </Container>
-    );
+      return (
+          <div>
+              {/* <h3>Lifts List</h3> */}
+              <Table striped bordered hover variant="dark">
+                  <thead>
+                      <tr>
+                          <th>Lift Name</th>
+                          <th>Reps</th>
+                          <th>PR</th>
+                          <th>Date</th>
+                          <th>Actions</th>
+
+                      </tr>
+                  </thead>
+                  <tbody>
+                      { this.liftList() }
+                  </tbody>
+              </Table>
+          </div>
+      )
   }
 }
 
-export default DataDisplay;
+// export default DataDisplay;

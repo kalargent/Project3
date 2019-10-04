da700b5adbf72d8923f285eb8abb9136dbf62c1b
jsf5077 <jsf5077@gmail.com>
2019-10-03 23:03:03 -0400

    Added a custom image for the Add PR Button

--- client/src/components/PRButton.js
+++ client/src/components/PRButton.js
@@ -1,22 +1,25 @@
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
+import Image from "react-bootstrap/Image";
+import Container from "react-bootstrap/Container";
 
 import React, { Component } from "react";
 
-
 class PRButton extends Component {
-
-  render () {
+  render() {
     return (
-
-        <Button className="m-2" variant="danger" id="button" onClick={() => this.props.toggle(true)}>
-          Add a New PR!
-        </Button>
-    )
-  };
-  
-
+      // <Button className="m-2" variant="danger" id="button" onClick={() => this.props.toggle(true)}>
+      //   Add a New PR!
+      // </Button>
+      <Container>
+        <Image
+          src={require("./images/NewPRButton.png")}
+          className="pr-image"
+          onClick={() => this.props.toggle(true)}
+          fluid="true"
+          />
+      </Container>
+    );
+  }
 }
-
-
 
 export default PRButton;
